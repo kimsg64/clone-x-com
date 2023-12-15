e35d06f4f250e1303ee7ce2afde7271b8c288c60
SeungGyu Kim <mushon66@naver.com>
2023-12-15 16:13:38 +0900

    default.tsx

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -5,6 +5,7 @@ type Props = { children: React.ReactNode; modal: React.ReactNode };
 export default function BeforLoginLayout({ children, modal }: Props) {
     return (
         <div className={styles.container}>
+            비포로그인
             {children}
             {modal}
         </div>
